5ae8e9f03b56f6666f4125c1023ddb404d73e958
KevinLaRocca <132600372+KevinLaRocca@users.noreply.github.com>
2026-03-25 15:01:05 -0300

    Delete while

--- consolidar.sh
+++ consolidar.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-while true; do
-    for f in "$HOME/EPNro1/entrada/"*.txt; do
-        if [ -f "$f" ]; then
-            cat "$f" >> "$HOME/EPNro1/salida/${FILENAME}.txt"
-            mv "$f" "$HOME/EPNro1/procesado/"
-        fi
-    done
-    sleep 3
+
+for f in "$HOME/EPNro1/entrada/"*.txt; do
+    if [ -f "$f" ]; then
+        cat "$f" >> "$HOME/EPNro1/salida/${FILENAME}.txt"
+        mv "$f" "$HOME/EPNro1/procesado/"
+    fi
 done
+
